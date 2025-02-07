b9b707f8cb4f3c0e38a817924f21ec15c6f90185
SimorangkirJonathan <143678943+SimorangkirJonathan@users.noreply.github.com>
2025-02-07 17:34:03 +0700

    Driver 2

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,14 +1,33 @@
 package fintech.driver;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23030-Simorangkir Jonathan
+ * @author 12S23046-Anastasya T.B. Siahaan
  */
 public class Driver2 {
 
     public static void main(String[] _args) {
 
-        // codes
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        String owner = scanner.nextLine();
+        String accountName = scanner.nextLine();
+
+        Account account = new Account(owner, accountName);
+
+        String command2 = scanner.nextLine();
+        String accountName2 = scanner.nextLine();
+        double amount = Double.parseDouble(scanner.nextLine());
+        String postedat = scanner.nextLine();
+        String note = scanner.nextLine();
+
+        Transaction transaction = new Transaction(0, accountName2, amount, postedat, note, account);
+
+        System.out.println(account);
+        System.out.println(transaction);
+
+        scanner.close();
 
     }
 
--- src/fintech/model/Transaction.java
+++ src/fintech/model/Transaction.java
@@ -1,11 +1,55 @@
 package fintech.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23030 - Simorangkir Jonathan
+ * @author 12S23046 - Anastasya T.B. Siahaan
  */
 public class Transaction {
 
-    // class definition
+    private static int nextId = 0;
+    private int id;
+    {
+        nextId++;
+        id = nextId;
+    }
+    private String accountName2;
+    private double amount;
+    private String postedat;
+    private String note;
+    private Account account;
+
+    public Transaction(int _id, String _accountName2, double _amount, String _postedat, String _note, Account account) {
+        this.accountName2 = _accountName2;
+        this.amount = _amount;
+        this.postedat = _postedat;
+        this.note = _note;
+        this.account = account;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAccountName() {
+        return accountName2;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getPostedat() {
+        return postedat;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "|" + this.accountName2 + "|" + this.amount + "|" + this.postedat + "|" + this.note + "|"
+                + (account.getBalance() + this.amount);
+    }
 
 }
