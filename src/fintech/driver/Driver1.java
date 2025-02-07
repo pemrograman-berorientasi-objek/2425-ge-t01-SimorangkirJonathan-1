741444d7c32769030915a57ea82ea6bc461718e5
SimorangkirJonathan <143678943+SimorangkirJonathan@users.noreply.github.com>
2025-02-07 16:33:20 +0700

    Pair 18

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,14 +1,32 @@
 package fintech.driver;
 
+import java.util.Scanner;
+
+import fintech.model.Account;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23030-Simorangkir jonathan
+ * @author 12S23046-Anastasya Tabitha Blessing Siahaan
  */
 public class Driver1 {
 
     public static void main(String[] _args) {
+        Scanner Command = new Scanner(System.in);
+        Scanner Owner = new Scanner(System.in);
+        Scanner Accountname = new Scanner(System.in);
+        double balance = 0.0;
 
-        // codes
+        String command = Command.nextLine();
+        String owner = Owner.nextLine();
+        String accountname = Accountname.nextLine();
+
+        if (command.equals("create-account")) {
+            System.out.println(owner + "|" + accountname + "|" + balance);
+        }
+
+        Command.close();
+        Owner.close();
+        Accountname.close();
 
     }
 
--- src/fintech/model/Account.java
+++ src/fintech/model/Account.java
@@ -1,11 +1,34 @@
 package fintech.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23030-Simorangkir jonathan
+ * @author 12s23046-Anastasya T.B Siahaan
  */
 public class Account {
+    private String owner;
+    private String accountname;
+    private double balance;
 
-    // class definition
+    public Account(String _owner, String _accountname) {
+        this.owner = _owner;
+        this.accountname = _accountname;
+        this.balance = 0.0;
+    }
 
+    public String getowner() {
+        return owner;
+    }
+
+    public String getaccountname() {
+        return accountname;
+    }
+
+    public double getbalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return this.owner + "|" + this.accountname + "|" + this.balance;
+    }
 }
