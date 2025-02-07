4db4752ec11c7092fc3b35ea7cf63144a8bfb7fe
SimorangkirJonathan <143678943+SimorangkirJonathan@users.noreply.github.com>
2025-02-07 17:50:20 +0700

    update task 2

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,28 @@
 package fintech.driver;
 
+import java.util.Scanner;
+import fintech.model.Transaction;
+
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @autor 12S23030-Simorangkir jonathan
+ * @autor 12S23046-Anastasya T.B Siahaan
  */
 public class Driver2 {
 
     public static void main(String[] _args) {
+        Scanner com = new Scanner(System.in);
 
-        // codes
+        String command = com.nextLine();
+        String transactionId = com.nextLine();
+        String accountName = com.nextLine();
+        double amount = com.nextDouble();
+        com.nextLine(); // consume the newline character
+        String type = com.nextLine();
 
+        if (command.equals("create-transaction")) {
+            Transaction transaction = new Transaction(transactionId, accountName, amount, type);
+            System.out.println(transaction.toString());
+        }
+        com.close();
     }
-
 }
--- src/fintech/model/Transaction.java
+++ src/fintech/model/Transaction.java
@@ -1,11 +1,39 @@
 package fintech.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author
  */
 public class Transaction {
+    private String transactionId;
+    private String accountName;
+    private double amount;
+    private String type; // "credit" or "debit"
 
-    // class definition
+    public Transaction(String transactionId, String accountName, double amount, String type) {
+        this.transactionId = transactionId;
+        this.accountName = accountName;
+        this.amount = amount;
+        this.type = type;
+    }
 
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return this.transactionId + "|" + this.accountName + "|" + this.amount + "|" + this.type;
+    }
 }
